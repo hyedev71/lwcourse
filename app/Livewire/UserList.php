02e2a25cee04ffcb6502ed1f5c713bc2d715254f
root <root@DESKTOP-159IP4Q>
2023-10-07 03:10:16 +0800

    Creating View User Modal.

--- app/Livewire/UserList.php
+++ app/Livewire/UserList.php
@@ -4,10 +4,29 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Livewire\WithPagination;
+//use Livewire\Attributes\On;
+//use Livewire\Attributes\Computed;
 
 class UserList extends Component
 {
+	use WithPagination;
+
 	public $search;
+
+	public $selectedUser;
+
+    public function mount()
+    {
+        $this->selectedUser = null; // Initialize the modal state
+    }
+
+	public function viewUser(User $user)	
+	{
+		$this->selectedUser = $user;
+
+		$this->dispatch('open-modal', name: 'user-details');
+	}
 
     public function render()
     {
